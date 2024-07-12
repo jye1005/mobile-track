2e16e71803747c0f4da58dcc185540e2bdf03a6c
jye._.1005 <jihye41q8@gmail.com>
2024-07-12 16:42:06 +0900

    Create chap02_assignment

---
+++ chap02.dart
@@ -0,0 +1,9 @@
+int SomeFn(var add, int number_one, int number_two) {
+  return add(number_one, number_two);
+}
+
+void main() {
+  var add = (int a, int b) => a + b;
+
+  print(SomeFn(add, 1, 2));
+}
