c61895586846ae8a4756f091623e33314d328e59
jye._.1005 <jihye41q8@gmail.com>
2024-07-19 23:05:13 +0900

    Create mobile-track chap04 assignment (future-callback)

---
+++ chap04_future-callback.dart
@@ -0,0 +1,33 @@
+Future<Map<String, String>> login() async {
+  return Future.delayed(
+      Duration(seconds: 2), () => {'id': 'user123', 'token': 'abc123'});
+}
+
+Future<Map<String, String>> fetchUserProfile() async {
+  return Future.delayed(
+      Duration(seconds: 1), () => {'id': 'John123', 'name': 'John'});
+}
+
+Future<String> fetchNotifications() async {
+  return Future.delayed(
+      Duration(seconds: 1, milliseconds: 500), () => '로그인 되었습니다.');
+}
+
+void main() {
+  print('Starting application...');
+
+  login().then((loginResult) {
+    print('Logged in: $loginResult');
+
+    Future userProfileFuture = fetchUserProfile();
+    Future notificationsFuture = fetchNotifications();
+
+    userProfileFuture.then((profile) {
+      print('User profile: $profile');
+    });
+
+    notificationsFuture.then((notifications) {
+      print('$notifications');
+    });
+  });
+}
